ad121d417eae5309cc0a93a7fb73f1b058bb1c79
Fredrik <ffredrik@gmail.com>
2014-10-03 12:17:47 +0200

    mere skit

--- op.py
+++ op.py
@@ -13,38 +13,56 @@ _log = logging.getLogger(__name__)
 class OpModule(Module):
     """A basic KitnIRC module which responds to messages."""
 
-    # This decorator tells KitnIRC what events to route to the
-    # function it decorates. The name of the function itself
-    # doesn't matter - call it what makes sense.
-    @Module.handle("PRIVMSG")
-    def respond(self, client, actor, recipient, message):
-        # Only pay attention if command starts with "!"
-        if not message.startswith("!"):
-            return
+    def add_command(self, client, command, event, helptext=None):
+        self.trigger_event("ADDCOMMAND", client, [command, event, helptext])
 
-        message = message.strip()
-        args = message.split(" ")
+    def remove_command(self, client, command, event):
+        self.trigger_event("REMOVECOMMAND", client, [command, event])
 
-        if len(args) == 2:
-          command = args[0]
-          argument = args[1]
+    @Module.handle("COMMANDS")
+    def register_commands(self, client, *args):
+        _log.info("Registering commands...")
+        self.add_command(client, "op", "OPUSER", "Give OP to user or self.")
 
-          #_log.info("Got command: %r - Argument: %r", args[0], args[1])
-          _log.info("%s wants to give %s OP in %s." % actor, argument, recipient)
-          #client.reply(recipient, actor, "Fin kommando! Kommandoen var %r og argumentet var %r." % (args[0], args[1]))
+    def unregister_commands(self, client):
+        self.remove_command(client, "op", "OPUSER")
 
-          return True
+    def start(self, *args, **kwargs):
+        self.register_commands(self.controller.client)
 
-        elif len(args) == 1:
-          command = args[0]
+    def stop(self, *args, **kwargs):
+        self.unregister_commands(self.controller.client)
 
-          _log.info("%s wants to be OP in %s." % (actor, recipient))
-
-          return True
-
+    @Module.handle('OPUSER')
+    def marketquote(self, client, actor, recipient, *args):
+        actor = User(actor)
+        if isinstance(recipient, Channel):
+            self.reply_to = recipient
         else:
-          _log.error("That's not right.")
-          return True
+            self.reply_to = actor
+        typeName = " ".join(args)
+
+        _log.info("%s wants to give OP in %s." % actor, recipient)
+        self.controller.client.msg(recipient, "takk %s, fin test." % actor)
+
+    # def respond(self, client, actor, recipient, message):
+    #
+    #       #_log.info("Got command: %r - Argument: %r", args[0], args[1])
+    #       _log.info("%s wants to give %s OP in %s." % actor, argument, recipient)
+    #       #client.reply(recipient, actor, "Fin kommando! Kommandoen var %r og argumentet var %r." % (args[0], args[1]))
+    #
+    #       return True
+    #
+    #     elif len(args) == 1:
+    #       command = args[0]
+    #
+    #       _log.info("%s wants to be OP in %s." % (actor, recipient))
+    #
+    #       return True
+    #
+    #     else:
+    #       _log.error("That's not right.")
+    #       return True
 
 
 # Let KitnIRC know what module class it should be loading.
